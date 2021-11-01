f6595e4266b1932e7ccc62b0be3b9e9c3b8086e7
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2021-10-31 23:17:15 -0400

    oculus input setup, materials converted to URP

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name){
+        Sound s = Array.Find(sounds, sound =>sound.name == name);
+        s.source.Play();
+    }
+}
