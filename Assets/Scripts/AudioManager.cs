97656aff2528827a386306ff1707412ad3b8a232
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2021-11-16 02:44:26 -0500

    audio working

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
+    public Sound[] sounds; 
 
     // Start is called before the first frame update
     void Awake()
